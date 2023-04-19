7e6e1f231319cb8b37b157e59ade123d195792f7
TareqJudehGithub <tareq.joudeh@gmail.com>
2023-04-19 22:37:07 +0300

    Completed Conditional lecture: If & Switch statements.

---
+++ CSharpCrash/Basics/Conditionals/Program.cs
@@ -0,0 +1,44 @@
+﻿//namespace Conditionals;
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Conditionals\n");
+
+    int myInt = 5;
+    int mySecondInt = 10;
+
+    string name = "john";
+    string upperName = "JOHN";
+
+    // Check if myInt is bigger than mySecondInt
+    if (myInt > mySecondInt) Console.WriteLine($"myInt > mySecondint ? Answer = {true}");
+    else Console.WriteLine($"myInt > mySecondint ? Answer = {false}");
+
+    System.Console.WriteLine("\n");
+
+    // Check if name equals upperName
+    if (name.Equals(upperName)) System.Console.WriteLine("Both string are equals");
+    else System.Console.WriteLine("String in both variables are not equals");
+    // Switch statements
+    string todayIs = "";
+    todayIs = "Thursday";
+    string answer;
+    switch (todayIs)
+    {
+      case "Wednesday":
+        answer = "Today is Wednesday";
+        break;
+      case "Thursday":
+        answer = " Today is Thursday\nIt is the weekend's Eve :)";
+        break;
+      default:
+        answer = "Invalid entry";
+        break;
+    }
+
+    System.Console.WriteLine(answer);
+  }
+
+
+}
